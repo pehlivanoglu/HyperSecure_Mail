6ae0a85e5815aab9d1e737616ddf2092960b530c
Ahmet Pehlivanoğlu <77580131+pehlivanoglu@users.noreply.github.com>
2023-12-16 03:48:59 +0300

    Delete server directory

--- server/api/authentication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    #path('verify_token', views.verify_token, name='verify_token'),
-    path('signin/', views.signin, name='signin'),
-    path('signup/', views.signup, name='signup'),
-]
